6625472fcb3a9fe0b67b9717a285374e5020f63d
Dehua Liu <53137698+seo888@users.noreply.github.com>
2023-04-22 06:27:04 +0000

    V1.4

--- target/www.yangtse.com/page/www.yangtse.com\js\bdtongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?9be02ba2b1490ca6bcfd52a9f9c7ed2f";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
